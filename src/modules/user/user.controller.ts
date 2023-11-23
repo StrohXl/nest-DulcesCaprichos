7f0b31a43bfe5a4276f80424e614edb150b1c6dd
StrohXl <xaviermayora20@gmail.com>
2023-11-23 19:14:40 -0400

    added useGuards in user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,12 +7,11 @@ import {
   Param,
   Delete,
   Query,
-  UseGuards
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '../auth/guards/jwt.guard';
-
 
 @Controller('user')
 export class UserController {
@@ -21,6 +20,7 @@ export class UserController {
   @Post('/confirm')
   confirmToken() {}
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query() query: any) {
     return this.userService.findAll(query);
@@ -32,11 +32,13 @@ export class UserController {
     return this.userService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
